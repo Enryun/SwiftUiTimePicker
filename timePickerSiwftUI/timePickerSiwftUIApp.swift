83959fd0343eefea347fccb4d64737e3854eba58
Enryun <baokieuha@gmail.com>
2022-05-06 16:38:41 +0700

    Initial Commit

---
+++ timePickerSiwftUI/timePickerSiwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  timePickerSiwftUIApp.swift
+//  timePickerSiwftUI
+//
+//  Created by James Thang on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct timePickerSiwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
