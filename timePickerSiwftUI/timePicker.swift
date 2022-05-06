45426eccf812b36775884c9dbd38b555b6fe7090
Enryun <baokieuha@gmail.com>
2022-05-06 16:46:11 +0700

    first commit

---
+++ timePickerSiwftUI/timePicker.swift
@@ -0,0 +1,20 @@
+//
+//  timePicker.swift
+//  timePickerSiwftUI
+//
+//  Created by James Thang on 06/05/2022.
+//
+
+import SwiftUI
+
+struct timePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct timePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        timePicker()
+    }
+}
